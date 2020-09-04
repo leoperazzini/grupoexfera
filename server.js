87eedf330f2bab46c63ddd96b77b04e6d2bf610b
Claudio <claudiohenriqueyoshii@hotmail.com>
2020-09-03 21:03:17 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const consign = require('consign')
+const forceDomain = require('forcedomain')
+
+const port = 3003
+
+app.set('view engine', 'ejs')
+app.set('views', './app/views')
+
+app.unsubscribe('/', express.static('public'))
+
+app.use(forceDomain({
+    hostname:'www.grupoexfera.com.br',
+    protocol: 'https'
+}))
+
+consign().include('./app/routes')
+.into(app)
+
+app.listen(port, ()=>{
+
+})
